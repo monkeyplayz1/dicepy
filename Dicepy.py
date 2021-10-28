340a3e9ec032f718eb5080f542db66ca410b0092
monkeyplayz1 <88561621+monkeyplayz1@users.noreply.github.com>
2021-10-28 12:18:56 +0100

    Update Dicepy.py

--- Dicepy.py
+++ Dicepy.py
@@ -1,9 +1,11 @@
 import random
-
+roll = 1,2,3,4,5,6,7,8,9,10
 
 print ('rolling dice....')
 print(random.randint(3, 10))
  
-print('dice has been rolled')
- 
+print('dice has been rolled!')
 
+if roll == 6:
+    #change the 6 to what you want
+ print('you have found the lucky number!')
